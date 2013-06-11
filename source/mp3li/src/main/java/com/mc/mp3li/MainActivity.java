937c1e2e8943a197ce99f7d889849a60dc1b3bf5
Paulo McNally <paulomcnally@gmail.com>
2013-06-11 00:26:26 -0700

    New changes

--- source/mp3li/src/main/java/com/mc/mp3li/MainActivity.java
+++ source/mp3li/src/main/java/com/mc/mp3li/MainActivity.java
@@ -25,10 +25,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class MainActivity extends Activity {
 
     private Api api = null;
-    private final ProgressDialog dialog = null;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,13 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         initClass();
         initSearchAction();
+
+        // instantiate it within the onCreate method
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setMessage("A message");
+        mProgressDialog.setIndeterminate(false);
+        mProgressDialog.setMax(100);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
     }
 
     private void initClass() {
@@ -101,14 +117,15 @@ public class MainActivity extends Activity {
                                     .findViewById(R.id.textViewArtist);
                             artist.setText(song.getString("artist") + " | " + song.getString("duration"));
 
-                            final String link = song.getString("link");
+                            final String song_title = song.getString("song");
+                            final String song_link = song.getString("link");
 
                             view_list.setOnClickListener(new View.OnClickListener() {
 
                                 @Override
                                 public void onClick(View v) {
 
-                                    showDialog();
+                                    showDialog(song_link, song_title);
                                     // Toast.makeText(getApplicationContext(), link, Toast.LENGTH_LONG).show();
 
                                 }
@@ -130,7 +147,10 @@ public class MainActivity extends Activity {
 
     }
 
-    private void showDialog() {
+    private void showDialog(String url, String title) {
+
+        final String my_url = url;
+        final String my_title = title;
         //Log.i(TAG, "show Dialog ButtonClick");
         AlertDialog.Builder builder =
                 new AlertDialog.Builder(this);
@@ -145,10 +165,16 @@ public class MainActivity extends Activity {
                             DialogInterface dialog,
                             int which) {
 
+                        switch (which) {
+                            case 0:
+                                DownloadFile downloadFile = new DownloadFile();
+                                downloadFile.execute(my_url, my_title);
+                                break;
+                        }
+
                     }
                 }).setNegativeButton(getString(R.string.dialog_cancel),
-                new DialogInterface.OnClickListener()
-                {
+                new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog,
                                         int which) {
@@ -158,5 +184,87 @@ public class MainActivity extends Activity {
         );
         AlertDialog alert = builder.create();
         alert.show();
+    }
+
+    // new DownloadFile().execute( String url, String song name )
+    private class DownloadFile extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... sUrl) {
+            try {
+                URL url = new URL(sUrl[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+
+                urlConnection.setDoOutput(true);
+                urlConnection.connect();
+
+                File SDCardRoot = new File("/sdcard/" + "result/");
+
+                File file = new File(SDCardRoot, "filename.format");
+
+                FileOutputStream fileOutput = new FileOutputStream(file);
+
+                InputStream inputStream = urlConnection.getInputStream();
+
+                //this is the total size of the file
+
+                int totalSize = urlConnection.getContentLength();
+
+                //variable to store total downloaded bytes
+
+                int downloadedSize = 0;
+
+                //create a buffer...
+
+                byte[] buffer = new byte[1024];
+
+                int bufferLength = 0; //used to store a temporary size of the buffer
+
+                //now, read through the input buffer and write the contents to the file
+
+                while ((bufferLength = inputStream.read(buffer)) > 0)
+
+                {
+
+                    //add the data in the buffer to the file in the file output stream (the file on the sd card
+
+                    fileOutput.write(buffer, 0, bufferLength);
+
+                    //add up the size so we know how much is downloaded
+
+                    downloadedSize += bufferLength;
+
+                    int progress = (int) (downloadedSize * 100 / totalSize);
+
+                    //this is where you would do something to report the prgress, like this maybe
+
+                    //updateProgress(downloadedSize, totalSize);
+                    publishProgress(progress);
+
+                }
+
+                fileOutput.close();
+
+
+            } catch (Exception e) {
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            super.onProgressUpdate(progress);
+            mProgressDialog.setProgress(progress[0]);
+        }
+
+        protected void onPostExecute(String result) {
+            mProgressDialog.dismiss();
+        }
     }
 }
