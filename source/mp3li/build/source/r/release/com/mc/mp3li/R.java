fa36fa88b0fbe5069c41ed31c54ee0e908ddf6e2
Paulo McNally <paulomcnally@gmail.com>
2013-06-10 19:27:21 -0700

    Request and load data in LinearLayout maquin request

--- source/mp3li/build/source/r/release/com/mc/mp3li/R.java
+++ source/mp3li/build/source/r/release/com/mc/mp3li/R.java
@@ -23,13 +23,16 @@ public final class R {
         public static final int logo=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080005;
         public static final int editTextSearch=0x7f080000;
-        public static final int imageButton=0x7f080001;
+        public static final int imageButtonSearch=0x7f080001;
         public static final int linearLayoutList=0x7f080002;
+        public static final int textViewArtist=0x7f080003;
+        public static final int textViewTitle=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int list=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -37,7 +40,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050000;
         public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int example=0x7f050002;
+        public static final int hello_world=0x7f050003;
     }
     public static final class style {
         /** 
--- source/mp3li/src/main/java/com/mc/mp3li/MainActivity.java
+++ source/mp3li/src/main/java/com/mc/mp3li/MainActivity.java
@@ -1,15 +1,50 @@
 package com.mc.mp3li;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mc.mp3li.tools.Api;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends Activity {
+
+    private Api api = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initClass();
+        initSearchAction();
+    }
+
+    private void initClass() {
+        api = new Api();
+    }
+
+    private void initSearchAction() {
+        final EditText search_text = (EditText) findViewById(R.id.editTextSearch);
+        ImageButton search_button = (ImageButton) findViewById(R.id.imageButtonSearch);
+        search_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AsyncRequest().execute(search_text.getText().toString());
+            }
+        });
     }
 
 
@@ -19,5 +54,68 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    private class AsyncRequest extends AsyncTask<String, String, JSONArray> {
+
+        protected void onPreExecute() {
+            // progressBarHome.setVisibility(View.VISIBLE);
+        }
+
+        protected JSONArray doInBackground(String... params) {
+            JSONArray list = api.request(params[0]);
+            return list;
+        }
+
+        protected void onProgressUpdate(String... message) {
+            //
+        }
+
+        protected void onPostExecute(JSONArray list_song) {
+            LinearLayout list_container = (LinearLayout) findViewById(R.id.linearLayoutList);
+            list_container.removeAllViews();
+            LayoutInflater inflater = (LayoutInflater) getApplicationContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            int i = 0;
+            if (list_song.length() > 0) {
+                for (i = 0; i < list_song.length(); i++) {
+                    final JSONObject song;
+                    try {
+                        song = list_song.getJSONObject(i);
+                        try {
+                            View view_list = inflater.inflate(R.layout.list, null);
+                            TextView title = (TextView) view_list
+                                    .findViewById(R.id.textViewTitle);
+                            title.setText(song.getString("song"));
+
+                            TextView artist = (TextView) view_list
+                                    .findViewById(R.id.textViewArtist);
+                            artist.setText(song.getString("artist"));
+
+                            final String link = song.getString("link");
+
+                            view_list.setOnClickListener(new View.OnClickListener() {
+
+                                @Override
+                                public void onClick(View v) {
+                                    Toast.makeText(getApplicationContext(), link, Toast.LENGTH_LONG).show();
+
+                                }
+
+                            });
+
+                            list_container.addView(view_list);
+                            i++;
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
 }
